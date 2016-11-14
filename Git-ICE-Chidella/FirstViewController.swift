b5eaf529dd5c3a3b8614280d9396fc75d50f1644
Chidella,Sarath Kumar <s525880@ch140001m.nwmissouri.edu>
2016-11-14 13:23:51 -0600

    Initial Commit

---
+++ Git-ICE-Chidella/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Git-ICE-Chidella
+//
+//  Created by Chidella,Sarath Kumar on 11/14/16.
+//  Copyright © 2016 Chidella,Sarath Kumar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Git-ICE-Chidella/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Git-ICE-Chidella
+//
+//  Created by Chidella,Sarath Kumar on 11/14/16.
+//  Copyright © 2016 Chidella,Sarath Kumar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
